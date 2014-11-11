1ebdcd2b638ab2d7022377eb791832ca91742ee6
svetlin-mollov <s.s.mollov@gmail.com>
2014-11-11 18:57:23 +0200

    Refactorinh.

--- Lab06/StorageExample/src/com/unisofia/fmi/storageexample/MainActivity.java
+++ Lab06/StorageExample/src/com/unisofia/fmi/storageexample/MainActivity.java
@@ -1,4 +1,3 @@
-
 package com.unisofia.fmi.storageexample;
 
 import java.io.File;
@@ -23,111 +22,113 @@ import com.unisofia.fmi.storageexample.util.SharedPrefsConsts;
 
 public class MainActivity extends Activity implements OnClickListener {
 
-    private EditText mContentEditText;
-    private Button mSharedPrefsButton;
-    private Button mInternalStorageButton;
-    private Button mExternalStorageButton;
-    private Button mOpenNextScreenButton;
+	private EditText mContentEditText;
+	private Button mSharedPrefsButton;
+	private Button mInternalStorageButton;
+	private Button mExternalStorageButton;
+	private Button mOpenNextScreenButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        mContentEditText = (EditText) findViewById(R.id.edittext_content);
-        mSharedPrefsButton = (Button) findViewById(R.id.button_shared_prefs);
-        mInternalStorageButton = (Button) findViewById(R.id.button_internal_storage);
-        mExternalStorageButton = (Button) findViewById(R.id.button_external_storage);
-        mOpenNextScreenButton = (Button) findViewById(R.id.button_next);
+		mContentEditText = (EditText) findViewById(R.id.edittext_content);
+		mSharedPrefsButton = (Button) findViewById(R.id.button_shared_prefs);
+		mInternalStorageButton = (Button) findViewById(R.id.button_internal_storage);
+		mExternalStorageButton = (Button) findViewById(R.id.button_external_storage);
+		mOpenNextScreenButton = (Button) findViewById(R.id.button_next);
 
-        mSharedPrefsButton.setOnClickListener(this);
-        mInternalStorageButton.setOnClickListener(this);
-        mExternalStorageButton.setOnClickListener(this);
-        mOpenNextScreenButton.setOnClickListener(this);
-    }
+		mSharedPrefsButton.setOnClickListener(this);
+		mInternalStorageButton.setOnClickListener(this);
+		mExternalStorageButton.setOnClickListener(this);
+		mOpenNextScreenButton.setOnClickListener(this);
+	}
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.button_shared_prefs:
-                writeInSharedPreferences();
-                break;
-            case R.id.button_internal_storage:
-                writeInInternalStorage();
-                break;
-            case R.id.button_external_storage:
-                writeInExternalStorage();
-                break;
-            case R.id.button_next:
-                openNextScreen();
-                break;
-        }
-    }
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.button_shared_prefs:
+			writeInSharedPreferences();
+			break;
+		case R.id.button_internal_storage:
+			writeInInternalStorage();
+			break;
+		case R.id.button_external_storage:
+			writeInExternalStorage();
+			break;
+		case R.id.button_next:
+			openNextScreen();
+			break;
+		}
+	}
 
-    private void writeInSharedPreferences() {
-        String text = mContentEditText.getText().toString();
-        SharedPreferences sharedPreferences = getSharedPreferences(SharedPrefsConsts.NAME,
-                Context.MODE_PRIVATE);
-        Editor editor = sharedPreferences.edit();
-        editor.putString(SharedPrefsConsts.KEY_TEXT, text);
-        editor.commit();
-        // OR
-        // editor.apply();
-        // What is the difference?
-    }
+	private void writeInSharedPreferences() {
+		String text = mContentEditText.getText().toString();
+		SharedPreferences sharedPreferences = getSharedPreferences(
+				SharedPrefsConsts.NAME, Context.MODE_PRIVATE);
+		Editor editor = sharedPreferences.edit();
+		editor.putString(SharedPrefsConsts.KEY_TEXT, text);
+		editor.commit();
+		// OR
+		// editor.apply();
+		// What is the difference?
+	}
 
-    private void writeInInternalStorage() {
-        String text = mContentEditText.getText().toString();
-        FileOutputStream fos = null;
-        try {
-            fos = openFileOutput(FileConsts.FILE_NAME, Context.MODE_PRIVATE);
-            fos.write(text.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (fos != null) {
-                try {
-                    fos.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
+	private void writeInInternalStorage() {
+		String text = mContentEditText.getText().toString();
+		FileOutputStream fos = null;
+		try {
+			fos = openFileOutput(FileConsts.FILE_NAME, Context.MODE_PRIVATE);
+			fos.write(text.getBytes());
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (fos != null) {
+				try {
+					fos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
-    private void writeInExternalStorage() {
-        String text = mContentEditText.getText().toString();
-        String state = Environment.getExternalStorageState();
-        if (!Environment.MEDIA_MOUNTED.equals(state)) {
-            Toast.makeText(this, "External storage not available!", Toast.LENGTH_SHORT).show();
-            return;
-        }
+	private void writeInExternalStorage() {
+		String text = mContentEditText.getText().toString();
+		String state = Environment.getExternalStorageState();
+		if (!Environment.MEDIA_MOUNTED.equals(state)) {
+			Toast.makeText(this, "External storage not available!",
+					Toast.LENGTH_SHORT).show();
+			return;
+		}
 
-        File sdCard = Environment.getExternalStorageDirectory();
-        File dir = new File(sdCard.getAbsolutePath() + File.separator + FileConsts.FOLDER_NAME);
-        dir.mkdirs();
-        File file = new File(dir, FileConsts.FILE_NAME);
+		File sdCard = Environment.getExternalStorageDirectory();
+		File dir = new File(sdCard.getAbsolutePath() + File.separator
+				+ FileConsts.FOLDER_NAME);
+		dir.mkdirs();
+		File file = new File(dir, FileConsts.FILE_NAME);
 
-        FileOutputStream fos = null;
-        try {
-            fos = new FileOutputStream(file);
-            fos.write(text.getBytes());
-            fos.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (fos != null) {
-                try {
-                    fos.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream(file);
+			fos.write(text.getBytes());
+			fos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (fos != null) {
+				try {
+					fos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
-    private void openNextScreen() {
-        Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
-    }
+	private void openNextScreen() {
+		Intent intent = new Intent(this, SecondActivity.class);
+		startActivity(intent);
+	}
 }
--- Lab06/StorageExample/src/com/unisofia/fmi/storageexample/SecondActivity.java
+++ Lab06/StorageExample/src/com/unisofia/fmi/storageexample/SecondActivity.java
@@ -1,4 +1,3 @@
-
 package com.unisofia.fmi.storageexample;
 
 import java.io.BufferedReader;
@@ -13,6 +12,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Environment;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -22,109 +22,121 @@ import android.widget.Toast;
 import com.unisofia.fmi.storageexample.util.FileConsts;
 import com.unisofia.fmi.storageexample.util.SharedPrefsConsts;
 
-
 public class SecondActivity extends Activity implements OnClickListener {
 
-    private TextView mContentTextView;
-    private Button mSharedPrefsButton;
-    private Button mInternalStorageButton;
-    private Button mExternalStorageButton;
+	private TextView mContentTextView;
+	private Button mSharedPrefsButton;
+	private Button mInternalStorageButton;
+	private Button mExternalStorageButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getActionBar().setDisplayShowHomeEnabled(true);
-        setContentView(R.layout.activity_second);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getActionBar().setDisplayShowHomeEnabled(true);
+		setContentView(R.layout.activity_second);
 
-        mContentTextView = (TextView) findViewById(R.id.textview_content);
-        mSharedPrefsButton = (Button) findViewById(R.id.button_shared_prefs);
-        mInternalStorageButton = (Button) findViewById(R.id.button_internal_storage);
-        mExternalStorageButton = (Button) findViewById(R.id.button_external_storage);
+		mContentTextView = (TextView) findViewById(R.id.textview_content);
+		mSharedPrefsButton = (Button) findViewById(R.id.button_shared_prefs);
+		mInternalStorageButton = (Button) findViewById(R.id.button_internal_storage);
+		mExternalStorageButton = (Button) findViewById(R.id.button_external_storage);
 
-        mSharedPrefsButton.setOnClickListener(this);
-        mInternalStorageButton.setOnClickListener(this);
-        mExternalStorageButton.setOnClickListener(this);
-    }
+		mSharedPrefsButton.setOnClickListener(this);
+		mInternalStorageButton.setOnClickListener(this);
+		mExternalStorageButton.setOnClickListener(this);
+	}
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.button_shared_prefs:
-                readFromSharedPreferences();
-                break;
-            case R.id.button_internal_storage:
-                readFromInternalStorage();
-                break;
-            case R.id.button_external_storage:
-                readFromExternalStorage();
-                break;
-        }
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			onBackPressed();
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-    private void readFromSharedPreferences() {
-        SharedPreferences sharedPreferences = getSharedPreferences(SharedPrefsConsts.NAME,
-                Context.MODE_PRIVATE);
-        String text = sharedPreferences.getString(SharedPrefsConsts.KEY_TEXT, "");
-        mContentTextView.setText(text);
-    }
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.button_shared_prefs:
+			readFromSharedPreferences();
+			break;
+		case R.id.button_internal_storage:
+			readFromInternalStorage();
+			break;
+		case R.id.button_external_storage:
+			readFromExternalStorage();
+			break;
+		}
+	}
 
-    private void readFromInternalStorage() {
-        StringBuilder sb = new StringBuilder();
-        FileInputStream fis = null;
-        BufferedReader br = null;
-        String line = null;
-        try {
-            fis = openFileInput(FileConsts.FILE_NAME);
-            br = new BufferedReader(new InputStreamReader(fis));
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+	private void readFromSharedPreferences() {
+		SharedPreferences sharedPreferences = getSharedPreferences(
+				SharedPrefsConsts.NAME, Context.MODE_PRIVATE);
+		String text = sharedPreferences.getString(SharedPrefsConsts.KEY_TEXT,
+				"");
+		mContentTextView.setText(text);
+	}
 
-        mContentTextView.setText(sb.toString());
-    }
+	private void readFromInternalStorage() {
+		StringBuilder sb = new StringBuilder();
+		FileInputStream fis = null;
+		BufferedReader br = null;
+		String line = null;
+		try {
+			fis = openFileInput(FileConsts.FILE_NAME);
+			br = new BufferedReader(new InputStreamReader(fis));
+			while ((line = br.readLine()) != null) {
+				sb.append(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 
-    private void readFromExternalStorage() {
-        String state = Environment.getExternalStorageState();
-        if (!Environment.MEDIA_MOUNTED.equals(state)) {
-            Toast.makeText(this, "External storage not available!", Toast.LENGTH_SHORT).show();
-            return;
-        }
+		mContentTextView.setText(sb.toString());
+	}
 
-        File sdCard = Environment.getExternalStorageDirectory();
-        File dir = new File(
-                sdCard.getAbsolutePath() + File.separator + FileConsts.FOLDER_NAME + File.separator + FileConsts.FILE_NAME);
+	private void readFromExternalStorage() {
+		String state = Environment.getExternalStorageState();
+		if (!Environment.MEDIA_MOUNTED.equals(state)) {
+			Toast.makeText(this, "External storage not available!",
+					Toast.LENGTH_SHORT).show();
+			return;
+		}
 
-        StringBuilder sb = new StringBuilder();
-        BufferedReader br = null;
-        String line = null;
-        try {
-            br = new BufferedReader(new FileReader(dir));
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+		File sdCard = Environment.getExternalStorageDirectory();
+		File dir = new File(sdCard.getAbsolutePath() + File.separator
+				+ FileConsts.FOLDER_NAME + File.separator
+				+ FileConsts.FILE_NAME);
 
-        mContentTextView.setText(sb.toString());
-    }
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = null;
+		String line = null;
+		try {
+			br = new BufferedReader(new FileReader(dir));
+			while ((line = br.readLine()) != null) {
+				sb.append(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		mContentTextView.setText(sb.toString());
+	}
 }
